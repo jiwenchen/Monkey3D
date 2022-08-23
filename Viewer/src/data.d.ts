e7ccadb7bf390d417ba3f0aa97e0530018b8e802
WANGKEQIAN <keqian.wang@biomind.ai>
2022-08-23 23:26:22 +0800

    feature: 调用接口显示图像

--- Viewer/src/data.d.ts
+++ Viewer/src/data.d.ts
@@ -1,10 +1,5 @@
 interface image3DStateType {
-  imageData: {
-    plane_type_vr: any;
-    plane_type_coronal: any;
-    plane_type_sagittal: any;
-    plane_type_axial: any;
-  };
+  imageData: any;
 }
 
 interface viewport3DStateType {
--- Viewer/src/pages/imageViewer/index.tsx
+++ Viewer/src/pages/imageViewer/index.tsx
@@ -1,9 +1,10 @@
 import { Layout } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './index.less';
 import ViewportGrid from './viewPanel/ViewportGrid';
-import type { Dispatch } from '@@/plugin-dva/connect';
+import type { Dispatch } from 'umi';
 import Tool from './leftPanel/Tool';
+import { useDispatch } from 'umi';
 const { Content, Sider } = Layout;
 
 interface ImageViewerProps {
@@ -11,7 +12,26 @@ interface ImageViewerProps {
   usToolsState: any;
 }
 
-const ImageViewer: React.FC<ImageViewerProps> = (props) => {
+const ImageViewer: React.FC<ImageViewerProps> = ({}) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: 'image3DModel/getVolumetype',
+      payload: { vol_type: 1 },
+      // @ts-ignore
+    }).then((res: string) => {
+      if (res === 'successful') {
+        [0, 1, 2].forEach((index) => {
+          dispatch({
+            type: 'image3DModel/getMprData',
+            payload: { plane_type: index },
+          });
+        });
+      }
+    });
+  }, []);
+
   return (
     <Layout className={styles.content}>
       <Sider width="100" theme={'light'}>
